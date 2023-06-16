0dadfdbb1fefe471ceb9b35b7435065db580499c
CarlosHugoRodriguezC <cahuroca.aw@gmail.com>
2023-06-16 13:20:01 -0600

    section 2 completed

--- 01-first-steps/app/page.tsx
+++ 01-first-steps/app/page.tsx
@@ -1,14 +1,16 @@
 import { Metadata } from "next";
+import NextLink from "next/link";
 
-export const metadata:Metadata = {
+export const metadata: Metadata = {
   title: "Home Page",
   description: "Created by Carlos",
-}
+};
 
 export default function Home() {
   return (
     <main className="flex  flex-col items-center  p-24">
       <span className="text-4xl">Hello World</span>
+      <NextLink href="/about">About Page</NextLink>
     </main>
   );
 }
